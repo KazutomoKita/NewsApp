820bed6276ee4f543cef076b6737558a9730ebaf
KazutomoKita <elliottstar77@yahoo.co.jp>
2020-04-20 14:27:46 +0900

    First Commit

---
+++ NewsApp/NewsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Kazutomo Kita on 2020/04/20.
+//  Copyright © 2020 Kazutomo Kita. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
